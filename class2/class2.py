6c853b63de1a0c980f57975e13f06b2325be930f
Arya-s-k <nayana.shashi@gmail.com>
2025-05-12 15:05:05 +0530

    Add files via upload

---
+++ class2/class2.py
@@ -0,0 +1,9 @@
+print("welcom to python")
+print(2+3)
+print("2+3=",2+3 )
+a=18
+b=18
+print(a)
+print("a=",a, end=" ")
+print("b=",b)
+print("name\tage\njoy\t12")
---
+++ class2/var.py
@@ -0,0 +1,8 @@
+x=100
+y=100
+z=x+y
+print(x,"+",y,"=",z)
+
+fname="Arya"
+lname=" S kolavi"
+print(fname + lname)
