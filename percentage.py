3ab874adfbc5d669735f468cf33c456131a9b14c
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 19:32:17 +0530

    Delete percentage.py

--- percentage.py
+++
@@ -1,9 +0,0 @@
-#percentage calculation
-math=int(input("enter your math marks:"))
-science=int(input("enter your science marks:"))
-english=int(input("enter your english marks:"))
-sst=int(input("enter your sst marks:"))
-sum=math+science+english+sst
-print("your total number of marks scored is = ",sum)
-percent=(sum/400)*100
-print("Your percentage is = ",percent,"%")
