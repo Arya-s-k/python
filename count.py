432e0aadd72ba54ca17d4a7d3e8542d99c8f9ac0
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 19:31:29 +0530

    Delete count.py

--- count.py
+++
@@ -1,8 +0,0 @@
-#program to count number of notes to give
-a=int(input("enter your amount:"))
-note_100=a//100
-note_50=(a%100)//50
-note_10=((a%100)%50)//10
-print("100 rupee notes",note_100)
-print("50 rupee notes",note_50)
-print("10 rupee notes",note_10)
