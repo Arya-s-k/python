260a4c0e7acadee00dfc86eec81cb70f2215a95b
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 19:31:38 +0530

    Delete greater_lesser.py

--- greater_lesser.py
+++
@@ -1,8 +0,0 @@
-num= int(input("enter a number :"))
-if num<15:
-    print(f"{num}is less than 15")
-    print("i'm in if block")
-else:
-    print(f"{num} is greater than 15")
-    print("im in else block")
-print("im outside both blocks")
