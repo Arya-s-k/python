15ffaac5f0515b330ffa60c5c891e89d1ed891a8
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 18:47:20 +0530

    Add files via upload

---
+++ and_or_not.py
@@ -0,0 +1,12 @@
+a=10
+b=5
+c=0
+if a and b and c :
+    print("all the values are true")
+else:
+    print("either one of your values is false")
+
+if a>0 or b>0:
+    print("any one value is greater than 0")
+else:
+    print("no value is greater than 0")
---
+++ bmi.py
@@ -0,0 +1,20 @@
+weight=float(input("enter your weight in kg: "))
+height=float(input("enter your height in m: "))
+
+BMI=weight / (height/100)**2
+
+if BMI>=18.5:
+    print("you are underweight")
+elif BMI>25:
+    print("you are healthy")
+elif BMI>30:
+    print("you are overweight")
+elif BMI>35:
+    print("you are severly overweight")
+elif BMI>40:
+    print("you are obese")
+else:
+    print("you are extermly obese ")
+   
+
+
