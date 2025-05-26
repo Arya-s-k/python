015c6e8d9aff7ede6d418f4bd592f0883284f817
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 19:29:50 +0530

    Delete acp4.py

--- acp4.py
+++
@@ -1,5 +0,0 @@
-temp=int(input("enter the temperature : "))
-if temp<20:
-    print(f"you should wear woolen clothes as your temperature is {temp}")
-else:
-    print(f"you should wear cotton clothes a your temperature is {temp}")
