7cb8b421aaef4ce0007cdf6fca76ac267c651677
Arya-s-k <nayana.shashi@gmail.com>
2025-05-26 19:34:18 +0530

    Delete ACP/acp1.py

--- ACP/acp1.py
+++
@@ -1,7 +0,0 @@
-raj="12/3/2013"
-ram="5/7/2012"
-shyam="6/2/2011"
-varun="18/4/2012"
-manu="9/4/2013"
-print("birthdays")
-print("raj=", raj, "\n", "ram=", ram, "\n", "shyam=", shyam, "\n", "varun=", varun, "\n", "manu=", manu)
