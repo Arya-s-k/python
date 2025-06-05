f68a81cff1bf62714058cfb3f199d6add2ccdfb6
Arya-s-k <nayana.shashi@gmail.com>
2025-06-05 20:06:32 +0530

    Add files via upload

---
+++ ACP/acp8.py
@@ -0,0 +1,10 @@
+#age 10-20
+age =int(input("enter age "))
+
+if age>=10:
+    if age<20:
+        print("age is between 10 and 20")
+    else:
+        print("age is grater than 20")
+else:
+    print("age is lesser than 10")
