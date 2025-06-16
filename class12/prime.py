7736fdf0219e663a9f0913a2e3b93a2a413b81a0
Arya-s-k <nayana.shashi@gmail.com>
2025-06-16 18:54:32 +0530

    Add files via upload

--- class12/prime.py
+++ class12/prime.py
@@ -4,8 +4,8 @@ lower = int(input (" enter the lower  limit "))
 for i in range(lower,upper+1):
         for j in range(2,i):
             if i % j == 0:
-                break;
-            else:
+                break
+        else:
                 print(i)
         
 
