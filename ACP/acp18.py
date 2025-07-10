55d2cf844d13c86dbdc922fc05ea0e119cef4190
Arya-s-k <nayana.shashi@gmail.com>
2025-07-10 19:58:28 +0530

    Add files via upload

---
+++ ACP/acp18.py
@@ -0,0 +1,9 @@
+import math
+
+x = int(input("enter a value for trignometric value "))
+
+print("the tan is ", math.tan(x))
+
+print("the  cos is  ", math.cos(x))
+
+print("the sin is ", math.sin(x))
