3e35e0110c28b83ce185315f95a2fdd33e10383d
Arya-s-k <nayana.shashi@gmail.com>
2025-08-14 19:44:26 +0530

    Add files via upload

---
+++ class26/tictactoe.py
@@ -0,0 +1,86 @@
+# tic tac toe game 
+
+board = {'7':' ' , '8':' ' , '9':' ',
+         '4':' ' , '5':' ' , '6':' ',
+         '1':' ' , '2':' ' , '3':' '
+         }
+
+board_keys = []
+
+for key in board :
+    board_keys.append(key)
+
+def print_board(board):
+    print(board["7"]+"|" + board["8"]+"|" + board["9"])
+    print("_|_|_")
+    print(board["4"]+"|" + board["5"]+"|" + board["6"])
+    print("_|_|_")
+    print(board["1"]+"|" + board["2"]+"|" + board["3"])
+    
+    
+def game():
+    
+    turn = "X"
+    count = 0
+    
+    for i in range(10):
+        print_board(board)
+        print("it is your turn",turn)
+        move = input("put in your place you want to move")
+        if board[move] == ' ' :
+            board[move] == turn
+            count+=1
+        else :
+            print("the board place is filled")
+            continue
+        
+        if count >= 5 :
+            if board[7] == board[8] == board[9]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[4] == board[5] == board[6]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[1] == board[2] == board[3]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[7] == board[4] == board[1]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[8] == board[5] == board[2]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[9] == board[6] == board[3]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[7] == board[5] == board[3]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+            elif board[9] == board[5] == board[1]:
+                print_board(board)
+                print("game over")
+                print(turn , " won ")
+        if count == 9 :
+            print("it is a tie")
+            
+        if turn == "X" :
+            turn = "O"
+        else : 
+            turn = "X"
+        
+    restart = input("do you want o restart (y / n )" )
+    if restart.lower() == "y":
+        for key in board_keys:
+            board[key] = " "
+            
+        game()
+
+if __name__ == "__main__" : 
+    game()
