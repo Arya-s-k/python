f6b31ebd0bbfdc42f5532313936098670812a604
Arya-s-k <nayana.shashi@gmail.com>
2025-08-14 19:59:32 +0530

    Add files via upload

--- class26/tictactoe.py
+++ class26/tictactoe.py
@@ -28,45 +28,53 @@ def game():
         print("it is your turn",turn)
         move = input("put in your place you want to move")
         if board[move] == ' ' :
-            board[move] == turn
+            board[move] = turn
             count+=1
         else :
             print("the board place is filled")
             continue
         
         if count >= 5 :
-            if board[7] == board[8] == board[9]:
+            if board['7'] == board['8'] == board['9'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[4] == board[5] == board[6]:
+                break
+            elif board['4'] == board['5'] == board['6'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[1] == board[2] == board[3]:
+                break
+            elif board['1'] == board['2'] == board['3'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[7] == board[4] == board[1]:
+                break
+            elif board['7'] == board['4'] == board['1'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[8] == board[5] == board[2]:
+                break
+            elif board['8'] == board['5'] == board['2'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[9] == board[6] == board[3]:
+                break
+            elif board['9'] == board['6'] == board['3'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[7] == board[5] == board[3]:
+                break
+            elif board['7'] == board['5'] == board['3'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
-            elif board[9] == board[5] == board[1]:
+                break
+            elif board['9'] == board['5'] == board['1'] !=1:
                 print_board(board)
                 print("game over")
                 print(turn , " won ")
+                break
         if count == 9 :
             print("it is a tie")
             
