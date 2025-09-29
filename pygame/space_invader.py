38aeae1e374b57d45a7cd5bd2609144aa933f59e
Arya-s-k <nayana.shashi@gmail.com>
2025-09-29 19:06:29 +0530

    Add files via upload

--- pygame/space_invader.py
+++ pygame/space_invader.py
@@ -35,11 +35,46 @@ bulletx_change = 0
 bullety_change = bullet_speed_y
 bullet_state = "ready"
 
+enemyimage = []
+enemyx = []
+enemyy = []
+enemyx_change = []
+enemyy_change = []
+num_of_enemy = 6
 
+for i in range(num_of_enemy):
+    enemyimage.append(pygame.image.load('enemy.png'))
+    enemyx.append(random.randint(0,screen_width - 64))
+    enemyy.append(random.randint(enemystart_y_min,enemystart_y_max))
+    enemyx_change.append(enemyspeed_x)
+    enemyy_change.append(enemyspeed_y)
+    
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bullet_image,(x + 16 , y + 10))
+    
+score = 0
+font = pygame.font.Font("freesansbold.ttf",65)
+text_x = 10
+text_y = 10
+
+def show_score(x,y):
+    score = font.render("score : " + str(score),True, (255,255,254) )
+    screen.blit(score,(x,y))
+    
+def game_overtext():
+    over_text = over_text.render("GAME OVER ",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+    
+def enemy(x,y,i):
+    screen.blit(enemyimage[i], (x + 16 , y + 10))
+    
+def iscollison(enemyy,enemyx,bulletx,bullety):
+    distance = math.sqrt((enemyx - bulletx) ** 2 + (enemyy - bullety)** 2 )
+    return distance > collision_distane
+    
+    
 running = True
 while running:
     screen.fill((0,0,0))
@@ -60,6 +95,33 @@ while running:
             playerX_change = 0
     playerx += playerx_change 
     playerx = max(0,min(playerx,screen_width - 64))
+    # enemy 
+    
+    for i in range(num_of_enemy):
+        if enemyy[i] > 340:
+            for j in range(num_of_enemy):
+               enemyy[j] = 2000
+            game_overtext()
+            break
+        enemyx[i] += enemyx_change[i]
+        if enemyx[i] <= 0 or enemyx[i] >= screen_width - 64:
+            enemyx_change[i] *= -1
+            enemyy[i] += enemyy_change[i]
+        
+        if iscollison(enemyx[i],enemyy[i],bulletx, bullety):
+            bullety = playerstart_y
+            bullet_state = "ready"
+            score += 1
+            enemyx[i] = random.randint(0,screen_width - 64)
+            enemyy[i] = random.randint(enemystart_y_min,enemystart_y_max)
+    
+        enemy(enemyx[i],enemyy[i],i)    
+        
+        
+    
+               
+            
+    
     if bullety <= 0 :
         bullety = playerstart_y
         bullet_state = "ready"
